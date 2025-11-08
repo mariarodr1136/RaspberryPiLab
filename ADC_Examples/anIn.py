c45532c4f33ae440441f270a3b50d7e856bf91b7
Maria Rodriguez <mrodr.contact@gmail.com>
2025-11-08 17:11:59 -0500

    Add anIn.py for reading analog values from ADC

---
+++ ADC_Examples/anIn.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import ADC0834
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+ADC0834.setup()
+
+try:
+    while True:
+        analogVal=ADC0834.getResult(0)
+        print(analogVal)
+        sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('GPIO cleanup completed.')
