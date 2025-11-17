16830b7f831b9d86776259b8e097ecfc67289b4b
Maria Rodriguez <mrodr.contact@gmail.com>
2025-11-17 16:03:29 -0500

    Refactor GPIO and keypad integration in script

--- Projects/kpLib-thread-al.py
+++ Projects/kpLib-thread-al.py
@@ -1,27 +1,38 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+PIRpin=12
+GPIO.setup(PIRpin,GPIO.IN)
 import LCD1602
-from kpLib import keypad
+import kpLib
 from time import sleep
 import threading
-myPad=keypad(returnChar='D')
+myPad=kpLib.keypad(returnChar='D')
 LCD1602.init(0x27,1)
 myString=""
 pwd='1234'
 def readKP():
     global myString
-    while True:
+    while myString != '*':
         myString=myPad.readKey()
         sleep(.2)
 readThread=threading.Thread(target=readKP,)
 readThread.daemon=True
 readThread.start()
-while True:
+while myString != '*':
     CMD=myString
     if CMD=='A'+pwd:
         LCD1602.write(0,0,"ARMED MODE      ")
+        moveVal=GPIO.input(PIRpin)
+        if moveVal==1:
+            LCD1602.write(0,1,"INTRUDER ALERT! ")
+        if moveVal==0:
+            LCD1602.write(0,1,"ALL CLEAR       ")
     if CMD=='B'+pwd:
         LCD1602.write(0,0,"DISARMED MODE   ")
+        LCD1602.write(0,1,"ALL SENSORS OFF ")
     if CMD=='C'+pwd:
         LCD1602.write(0,0,"PASSWORD?     ")
+        LCD1602.write(0,1,"                ")
         while myString=='C'+pwd:
             pass
         pwd=myString
@@ -29,3 +40,7 @@ while True:
         LCD1602.write(0,1, pwd)
         sleep(2)
         LCD1602.clear()
+sleep(1)
+GPIO.cleanup()
+LCD1602.clear()
+print("Exiting program.")
